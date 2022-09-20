38ca12c8e625ca0bcb472c6e1ab13a6e4f6c98c7
gautamsen117 <gautamsen117@gmail.com>
2022-09-20 20:15:24 +0600

    switch-case

---
+++ switch-case.js
@@ -0,0 +1,18 @@
+const grade = 'A+';
+switch(grade){
+    case 'A+':
+        console.log('Good Job');
+        break;
+    case 'A':
+        console.log('You got A grade');
+        break;
+    case 'B':
+        console.log('Your grade is B');
+        break;
+    case 'F':
+        console.log('Fail this exam');
+        break;
+    default:
+        console.log('Your result is not find');
+        break;
+}
