b3f197d204550e8b4f30a0111a100b9e74aaa67a
gautamsen117 <gautamsen117@gmail.com>
2022-09-20 20:08:02 +0600

    total price

---
+++ total-price.js
@@ -0,0 +1,22 @@
+const a = {
+    name: 'laptop',
+    color: 'black',
+    price: 12000
+}
+const b = {
+    name: 'monitor',
+    color: 'gray',
+    price: 5000
+}
+const c = {
+    name: 'mobile',
+    color: 'black',
+    price: 2500
+}
+
+// console.log(a.price);
+// console.log(b.price);
+// console.log(c.price);
+
+const totalPrice = a.price + b.price + c.price;
+console.log(totalPrice);
