483a2fb06cbcc3def9d4b6ad00194c2cb03cfc4c
gautamsen117 <gautamsen117@gmail.com>
2022-09-20 19:25:51 +0600

    odd-even

---
+++ odd-even.js
@@ -0,0 +1,23 @@
+// check odd and even number with no return
+function oddEven(number){
+    if(number % 2 == 0){
+        console.log('This is even number')
+    }
+    else{
+        console.log('This is odd number');
+    }
+}
+oddEven(19) ;
+oddEven(20); 
+// check even and odd number with retrun
+
+function checkEvenOdd(number){
+    if(number % 2 == 0){
+        return 'Its even';
+    }
+    else{
+        return 'Its odd';
+    }
+}
+
+let checkNumber = checkEvenOdd(25);
