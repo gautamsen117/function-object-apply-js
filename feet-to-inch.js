21f56c77d30c846b782f57a0154423776ae660e5
gautamsen117 <gautamsen117@gmail.com>
2022-09-20 20:55:52 +0600

    feet-to-inch

--- feet-to-inch.js
+++ feet-to-inch.js
@@ -1,3 +1,7 @@
+/* 
+Write a function that will take feet as the input parameter and will convert it into inch and will return the result in inch.
+*/
+
 function feetToInch(feet){
     const inch = feet * 12;
     return inch;
